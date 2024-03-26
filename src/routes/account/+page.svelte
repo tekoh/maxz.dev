f7e52f9fb0482d29bafb4ad2ae597e8608391eb7
max <29782397+tekoh@users.noreply.github.com>
2024-03-26 22:41:28 +0000

    max height container!!

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -17,24 +17,26 @@
       <h1 class="text-center text-3xl text-primary">{data.user?.id}</h1>
       <p>created on {dayjs(data.user?.createdAt).format("YYYY-MM-DD")}</p>
     {:else}
-      {#if enterLoginToken}
-        <form action="?/login" method="post" in:fade={{ delay: 200, duration: 200 }}>
-          <input
-            type="password"
-            autofocus
-            placeholder="access token"
-            class="w-full rounded-lg border border-primary border-opacity-25 bg-background p-2 font-medium duration-300 placeholder:text-text placeholder:opacity-50 focus:border-opacity-100 focus:outline-none"
-          />
-        </form>
-      {:else}
-        <button
-          out:fade={{ duration: 200 }}
-          on:click={() => (enterLoginToken = true)}
-          class="w-full rounded-lg bg-primary p-2 font-medium text-black duration-300 hover:opacity-75"
-        >
-          log in
-        </button>
-      {/if}
+      <div class="max-h-[42px]">
+        {#if enterLoginToken}
+          <form action="?/login" method="post" in:fade={{ delay: 100, duration: 100 }}>
+            <input
+              type="password"
+              autofocus
+              placeholder="access token"
+              class="w-full rounded-lg border border-primary border-opacity-25 bg-background p-2 font-medium duration-300 placeholder:text-text placeholder:opacity-50 focus:border-opacity-100 focus:outline-none"
+            />
+          </form>
+        {:else}
+          <button
+            out:fade={{ duration: 100 }}
+            on:click={() => (enterLoginToken = true)}
+            class="w-full rounded-lg border border-background border-opacity-0 bg-primary p-2 font-medium text-black duration-300 hover:opacity-75"
+          >
+            log in
+          </button>
+        {/if}
+      </div>
 
       <form action="?/register" method="post">
         <button
