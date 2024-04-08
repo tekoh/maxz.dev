ed101a53c3a8488293ceb624125f98180f6c3368
max <29782397+tekoh@users.noreply.github.com>
2024-04-08 23:04:41 +0100

    paranoid schizophrenic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,4 @@
 </svelte:head>
 
 <h1 class="mt-40 w-full text-center text-6xl font-bold text-primary sm:text-8xl">maxz.dev</h1>
+<p class="-mt-2 text-center text-xs text-secondary">paranoid schizophrenic</p>
