b1e93f274f9e052b9a41396bac933f3e272c028b
max <29782397+tekoh@users.noreply.github.com>
2024-04-10 17:48:56 +0100

    use svgs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
-  import telegram from "$lib/assets/telegram.png?as=run:0";
-  import discord from "$lib/assets/discord.png?as=run:0";
-  import Img from "@zerodevx/svelte-img";
+  import Discord from "$lib/components/icons/discord.svelte";
+  import Telegram from "$lib/components/icons/telegram.svelte";
 </script>
 
 <svelte:head>
@@ -9,12 +8,17 @@
 </svelte:head>
 
 <h1 class="mt-40 w-full text-center text-6xl font-bold text-primary sm:text-8xl">maxz.dev</h1>
-<p class="-mt-2 text-center text-xs text-secondary">paranoid schizophrenic</p>
+<p class="-mt-2 text-center text-xs text-accent">paranoid schizophrenic</p>
 <div class="mt-8 flex w-full items-center justify-center gap-4">
-  <a href="https://t.me/maxmeoww" target="_blank">
-    <Img src={telegram} class="w-8" />
+  <a href="https://t.me/maxmeoww" target="_blank" class="tooltip block" data-tip="telegram">
+    <Telegram class="w-12 fill-secondary" />
   </a>
-  <a href="https://discord.com/users/672793821850894347" target="_blank">
-    <Img src={discord} class="w-8" />
+  <a
+    href="https://discord.com/users/672793821850894347"
+    target="_blank"
+    class="tooltip block"
+    data-tip="discord"
+  >
+    <Discord class="w-12 fill-secondary" />
   </a>
 </div>
