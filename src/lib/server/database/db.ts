d654dde205ec9b722a948a3ce3270679642d2c7b
max <29782397+mxz7@users.noreply.github.com>
2024-06-23 15:13:38 +0100

    remove bulk

--- src/lib/server/database/db.ts
+++
@@ -1,9 +0,0 @@
-import { DB_TOKEN, DB_URL } from "$env/static/private";
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
-
-const client = createClient({ authToken: DB_TOKEN, url: DB_URL });
-
-const db = drizzle(client);
-
-export default db;
--- src/routes/account/+page.server.ts
+++
@@ -1,8 +0,0 @@
-export async function load({ locals }) {
-  const auth = await locals.validate();
-
-  if (auth) {
-    return { authenticated: true, user: auth.user };
-  }
-  return { authenticated: false };
-}
